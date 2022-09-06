556ec567ff0ef03c3a392dff2a46643a154ecf75
Usha Sai Chintha <ushasai.chintha@thoughworks.com>
2022-09-06 22:38:38 +0530

    Initial Commit

---
+++ ClimaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClimaApp
+//
+//  Created by Usha Sai Chintha on 06/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
